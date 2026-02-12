f8e497dac962d03d5c1b47b8b2efedd131427a13
Jonathan McDill <jonathan@vaultinnovation.com>
2026-02-12 14:57:53 -0500

    Fix: Enable Server mode for Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [react(), markdoc(), keystatic()],
   adapter: netlify(),
 });
+
