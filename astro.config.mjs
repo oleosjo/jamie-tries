fa081a711c1d3c0a9797bd38c54f362afc064229
Jonathan McDill <jonathan@vaultinnovation.com>
2026-02-12 14:35:48 -0500

    Fix: Add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
+  adapter: netlify(),
 });
