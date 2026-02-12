5180a2b7c53e2b20f082c64da6cc76823766830b
Jonathan McDill <jonathan@vaultinnovation.com>
2026-02-12 14:43:11 -0500

    Enable GitHub storage for production

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,23 +1,28 @@
-import { config, fields, collection } from '@keystatic/core';
+import { config, fields, collection } from "@keystatic/core";
 
 export default config({
-  storage: {
-    kind: 'local',
-  },
+  storage: import.meta.env.PROD
+    ? {
+        kind: "github",
+        repo: "oleosjo/jamie-tries", // <-- CHANGE THIS to your actual repo info!
+      }
+    : {
+        kind: "local",
+      },
   collections: {
     posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'src/content/posts/*',
-      format: { contentField: 'content' },
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*",
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
+        title: fields.slug({ name: { label: "Title" } }),
         content: fields.markdoc({
-          label: 'Content',
+          label: "Content",
           options: {
             image: {
-              directory: 'src/assets/images/posts',
-              publicPath: '../../assets/images/posts/',
+              directory: "src/assets/images/posts",
+              publicPath: "../../assets/images/posts/",
             },
           },
         }),
